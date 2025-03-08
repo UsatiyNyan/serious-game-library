d9c1ec8daaaa9f9a534a7e566cd85b50d4f37410
UsatiyNyan <us4tiyny4n@gmail.com>
2025-03-08 20:37:14 +0100

    graphics: transform: decompose from mat4, scale

--- include/sl/game/graphics/component/transform.hpp
+++ include/sl/game/graphics/component/transform.hpp
@@ -1,5 +1,9 @@
 //
 // Created by usatiynyan.
+// TODO:
+// - use scale
+// - skew and perspective in decompose
+// - transform component should be a variant<{tr, rot, s, ...}, mat4>
 //
 
 #pragma once
@@ -8,16 +12,32 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/matrix_decompose.hpp>
 #include <glm/gtx/quaternion.hpp>
+#include <sl/meta/monad/maybe.hpp>
 
 namespace sl::game {
 
 struct transform {
     glm::vec3 tr;
     glm::quat rot = glm::identity<glm::quat>();
+    glm::vec3 s{ 1.0f, 1.0f, 1.0f }; // use this component
+
+    static meta::maybe<transform> from_mat4(const glm::mat4& mat4) {
+        transform result;
+        // TODO: skew and perspective
+        glm::vec3 skew;
+        glm::vec4 perspective;
+        if (!glm::decompose(mat4, result.s, result.rot, result.tr, skew, perspective)) {
+            return meta::null;
+        }
+        return result;
+    }
 
     void translate(const glm::vec3& a_tr) { tr += a_tr; }
     void rotate(const glm::quat& a_rot) { rot = glm::normalize(a_rot * rot); }
+    void scale(const glm::vec3& a_s) { s = a_s; }
+
     void normalize() {
         if (glm::length2(tr) > 0.0f) {
             tr = glm::normalize(tr);
@@ -34,6 +54,10 @@ inline transform rotate(transform tf, const glm::quat& rot) {
     tf.rotate(rot);
     return tf;
 }
+inline transform scale(transform tf, const glm::vec3& tr) {
+    tf.scale(tr);
+    return tf;
+}
 inline transform normalize(transform tf) {
     tf.normalize();
     return tf;
@@ -42,6 +66,7 @@ inline transform combine(const transform& a, const transform& b) {
     return transform{
         .tr = b.rot * a.tr + b.tr,
         .rot = b.rot * a.rot,
+        .s = b.s * a.s,
     };
 }
 
