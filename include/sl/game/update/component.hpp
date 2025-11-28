5e6c47812d8a88f696ad1b5fd52fa6e13e86daa5
UsatiyNyan <us4tiyny4n@gmail.com>
2025-11-29 00:00:58 +0100

    game: update: fix layer

--- include/sl/game/update/component.hpp
+++ include/sl/game/update/component.hpp
@@ -4,8 +4,10 @@
 
 #pragma once
 
-#include "sl/game/layer.hpp"
 #include "sl/game/time.hpp"
+
+#include <sl/ecs/layer.hpp>
+#include <sl/meta/func/function.hpp>
 
 namespace sl::game {
 
@@ -13,38 +15,34 @@ struct node {
     entt::entity parent = entt::null;
     std::deque<entt::entity> children{}; // amortized insert/remove/iter
 
-    template <GameLayerRequirements Layer>
     static void attach_child(
-        Layer& layer,
+        ecs::layer_view lv,
         entt::entity parent_entity,
         node& parent_node,
         entt::entity child_entity,
         node& child_node
     ) {
-        entt::registry& registry = layer.registry;
+        entt::registry& registry = lv.registry;
         DEBUG_ASSERT(registry.try_get<node>(parent_entity) == &parent_node);
         DEBUG_ASSERT(registry.try_get<node>(child_entity) == &child_node);
         attach_child_impl(parent_entity, parent_node, child_entity, child_node);
     }
 
-    template <GameLayerRequirements Layer>
-    static void attach_child(Layer& layer, entt::entity parent_entity, entt::entity child_entity) {
-        entt::registry& registry = layer.registry;
-        node& parent_node = registry.get_or_emplace<node>(parent_entity);
-        node& child_node = registry.get_or_emplace<node>(child_entity);
+    static void attach_child(ecs::layer_view lv, entt::entity parent_entity, entt::entity child_entity) {
+        node& parent_node = lv.registry.get_or_emplace<node>(parent_entity);
+        node& child_node = lv.registry.get_or_emplace<node>(child_entity);
         attach_child_impl(parent_entity, parent_node, child_entity, child_node);
     }
 
-    template <GameLayerRequirements Layer, std::size_t children_span>
+    template <std::size_t children_span>
     static void attach_children(
-        Layer& layer,
+        ecs::layer_view lv,
         entt::entity parent_entity,
         std::span<const entt::entity, children_span> child_entities
     ) {
-        entt::registry& registry = layer.registry;
-        node& parent_node = registry.get_or_emplace<node>(parent_entity);
+        node& parent_node = lv.registry.get_or_emplace<node>(parent_entity);
         for (const entt::entity child_entity : child_entities) {
-            node& child_node = registry.get_or_emplace<node>(child_entity);
+            node& child_node = lv.registry.get_or_emplace<node>(child_entity);
             attach_child_impl(parent_entity, parent_node, child_entity, child_node);
         }
     }
@@ -60,7 +58,6 @@ private:
     }
 };
 
-template <GameLayerRequirements Layer>
-using update = function<void(Layer&, entt::entity, time_point)>;
+using update = meta::unique_function<void(ecs::layer_view, entt::entity, time_point)>;
 
 } // namespace sl::game
--- include/sl/game/update/system.hpp
+++ include/sl/game/update/system.hpp
@@ -4,35 +4,38 @@
 
 #pragma once
 
-#include "sl/game/layer.hpp"
 #include "sl/game/update/component.hpp"
 
 #include <libassert/assert.hpp>
+#include <sl/ecs/layer.hpp>
+#include <type_traits>
 
 namespace sl::game {
+
+template <typename F>
+concept Update = std::is_invocable_r_v<void, F, ecs::layer_view, entt::entity, time_point>;
+
 namespace detail {
 
-template <GameLayerRequirements Layer>
-void tree_update_top_down(Layer& layer, update<Layer> update, time_point time_point) {
-    std::deque<entt::entity> queue{ layer.root };
+void tree_update_top_down(ecs::layer_view lv, Update auto an_update, time_point time_point) {
+    std::deque<entt::entity> queue{ lv.root };
 
     while (!queue.empty()) {
         const entt::entity node_entity = queue.front();
         queue.pop_front();
 
-        if (const node* maybe_node_component = layer.registry.template try_get<node>(node_entity);
+        if (const node* maybe_node_component = lv.registry.template try_get<node>(node_entity);
             maybe_node_component != nullptr) {
             const node& node_component = *maybe_node_component;
             queue.insert(queue.end(), node_component.children.begin(), node_component.children.end());
         }
 
-        update(layer, node_entity, time_point);
+        an_update(lv, node_entity, time_point);
     }
 }
 
-template <GameLayerRequirements Layer>
-void tree_update_bottom_up(Layer& layer, update<Layer> update, time_point time_point) {
-    std::vector<entt::entity> tmp{ layer.root };
+void tree_update_bottom_up(ecs::layer_view lv, Update auto an_update, time_point time_point) {
+    std::vector<entt::entity> tmp{ lv.root };
     std::vector<entt::entity> accum;
 
     while (!tmp.empty()) {
@@ -40,7 +43,7 @@ void tree_update_bottom_up(Layer& layer, update<Layer> update, time_point time_p
         tmp.pop_back();
         accum.push_back(node_entity);
 
-        if (const node* maybe_node_component = layer.registry.template try_get<node>(node_entity);
+        if (const node* maybe_node_component = lv.registry.template try_get<node>(node_entity);
             maybe_node_component != nullptr) {
             const node& node_component = *maybe_node_component;
             tmp.insert(tmp.end(), node_component.children.begin(), node_component.children.end());
@@ -49,7 +52,7 @@ void tree_update_bottom_up(Layer& layer, update<Layer> update, time_point time_p
 
     for (auto r_it = accum.rbegin(); r_it != accum.rend(); ++r_it) {
         const entt::entity node_entity = *r_it;
-        update(layer, node_entity, time_point);
+        an_update(lv, node_entity, time_point);
     }
 }
 
@@ -60,25 +63,23 @@ enum class tree_update_order {
     BOTTOM_UP, // visit children first
 };
 
-template <GameLayerRequirements Layer>
-void tree_update_system(tree_update_order order, Layer& layer, update<Layer> update, time_point time_point) {
+void tree_update_system(tree_update_order order, ecs::layer_view lv, Update auto an_update, time_point time_point) {
     switch (order) {
     case tree_update_order::TOP_DOWN:
-        detail::tree_update_top_down(layer, std::move(update), time_point);
+        detail::tree_update_top_down(lv, std::move(an_update), time_point);
         break;
     case tree_update_order::BOTTOM_UP:
-        detail::tree_update_bottom_up(layer, std::move(update), time_point);
+        detail::tree_update_bottom_up(lv, std::move(an_update), time_point);
         break;
     default:
-        PANIC();
+        UNREACHABLE();
     }
 }
 
-template <GameLayerRequirements Layer>
-void update_system(Layer& layer, time_point time_point) {
-    auto entities = layer.registry.template view<update<Layer>>();
-    for (auto&& [entity, update] : entities.each()) {
-        update(layer, entity, time_point);
+inline void update_system(ecs::layer_view lv, time_point time_point) {
+    auto entities = lv.registry.template view<update>();
+    for (auto&& [entity, an_update] : entities.each()) {
+        an_update(lv, entity, time_point);
     }
 }
 
