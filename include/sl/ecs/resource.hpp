1f574700809350d6d9c377655086532a87eb0fca
UsatiyNyan <us4tiyny4n@gmail.com>
2025-12-05 01:37:42 +0100

    ecs: resource: simplify, remove registry

--- include/sl/ecs/resource.hpp
+++ include/sl/ecs/resource.hpp
@@ -12,6 +12,7 @@
 #include <sl/exec/algo/sched/start_on.hpp>
 #include <sl/exec/algo/sync/mutex.hpp>
 #include <sl/exec/coro/async.hpp>
+#include <sl/exec/coro/await.hpp>
 #include <sl/exec/model/concept.hpp>
 #include <sl/meta/monad/maybe.hpp>
 #include <sl/meta/storage/persistent.hpp>
@@ -22,65 +23,26 @@
 namespace sl::ecs {
 
 template <typename T>
+struct resource;
+
+template <typename T>
 struct resource : meta::unique {
+    using ptr_type = std::unique_ptr<resource<T>>;
     using storage_type = meta::persistent_storage<meta::unique_string, T>;
 
 public: // creation
-    struct make_result {
-        resource& value;
-        bool is_emplaced;
-    };
-
     // executor is expected to be manual or serial, guaranteeing safety to access registry
-    static make_result make_root(entt::registry& registry, exec::executor& executor, entt::entity on) {
-        return make_impl(registry, executor, on, meta::null);
+    static ptr_type make(exec::executor& executor, resource* parent = nullptr) {
+        return ptr_type{ new resource{
+            typename storage_type::init_type{
+                .get_parent = [parent]() -> storage_type* { return parent == nullptr ? nullptr : &parent->storage_; },
+            },
+            executor,
+        } };
     }
 
-    make_result make_child(entt::registry& registry, entt::entity on) & {
-        return make_impl(registry, executor_, on, entity_);
-    }
-
-private:
-    static make_result make_impl(
-        entt::registry& registry,
-        exec::executor& executor,
-        entt::entity entity,
-        meta::maybe<entt::entity> parent
-    ) {
-        if (resource* present = registry.try_get<resource<T>>(entity); present != nullptr) {
-            return make_result{
-                .value = *present,
-                .is_emplaced = false,
-            };
-        }
-        typename storage_type::init_type storage_init;
-        if (parent.has_value()) {
-            storage_init.get_parent = [&registry, p_entity = parent.value()] {
-                auto* p_ptr = registry.try_get<resource<T>>(p_entity);
-                DEBUG_ASSERT(p_ptr, "parent resource not found");
-                return p_ptr;
-            };
-        }
-        return make_result{
-            .value = registry.emplace<resource<T>>(
-                entity,
-                // args:
-                registry,
-                executor,
-                entity,
-                std::move(storage_init)
-            ),
-            .is_emplaced = true,
-        };
-    }
-
-    resource(
-        entt::registry& registry,
-        exec::executor& executor,
-        entt::entity entity,
-        typename storage_type::init_type storage_init
-    )
-        : storage_{ std::move(storage_init) }, executor_{ executor }, registry_{ registry }, entity_{ entity } {}
+    resource(typename storage_type::init_type storage_init, exec::executor& executor)
+        : storage_{ std::move(storage_init) }, executor_{ executor } {}
 
 public: // behaviour
     using reference_type = meta::persistent<T>;
@@ -97,12 +59,14 @@ public: // behaviour
     // during load
     exec::async<meta::maybe<reference_type>>
         require(meta::unique_string id, auto loader, bool override_after_load = true) & {
+        using exec::operator co_await;
+
         if (auto maybe_value = storage_.lookup(id)) {
             co_return maybe_value.value();
         }
 
         {
-            const auto [promises_it, promises_is_emplaced] = promises_by_id_.emplace(id);
+            const auto [promises_it, promises_is_emplaced] = promises_by_id_.try_emplace(id);
             if (!promises_is_emplaced) {
                 // awaiter context
                 auto [future, promise] = exec::make_contract<reference_type, meta::unit>();
@@ -118,20 +82,16 @@ public: // behaviour
 
         // loader context
 
-        // caching before awaiting loader
-        entt::registry& registry = registry_;
-        entt::entity entity = entity_;
+        meta::maybe<T> maybe_loaded_value = co_await std::move(loader);
+        co_await exec::start_on(executor_);
 
-        meta::maybe<T> maybe_loaded_value = co_await std::move(loader) | exec::continue_on(executor_);
-        DEBUG_ASSERT(registry.try_get<resource<T>>(entity) == this, "loader outlived resource");
-
-        auto maybe_reference =
-            std::move(maybe_loaded_value).map([this, override_after_load](T value) -> reference_type {
+        meta::maybe<reference_type> maybe_reference =
+            std::move(maybe_loaded_value).map([this, id, override_after_load](T value) -> reference_type {
                 if (override_after_load) {
-                    auto [reference, _] = storage_.insert(std::move(value));
+                    auto [reference, _] = storage_.insert(id, std::move(value));
                     return reference;
                 } else {
-                    auto [reference, _] = storage_.emplace(std::move(value));
+                    auto [reference, _] = storage_.emplace(id, std::move(value));
                     return reference;
                 }
             });
@@ -142,11 +102,13 @@ public: // behaviour
         std::vector<promise_type> promises = std::move(promises_it.value());
         promises_by_id_.erase_fast(promises_it);
 
-        for (promise_type& promise : promises) {
-            if (maybe_reference.has_value()) {
-                promise.set_value(maybe_reference.value());
-            } else {
-                promise.set_error(meta::null);
+        if (maybe_reference.has_value()) {
+            for (promise_type& promise : promises) {
+                promise.set_value(reference_type{ maybe_reference.value() });
+            }
+        } else {
+            for (promise_type& promise : promises) {
+                promise.set_error(meta::unit{});
             }
         }
 
@@ -180,10 +142,8 @@ public: // behaviour
 
 private:
     storage_type storage_;
-    tsl::robin_map<meta::unique_string, std::vector<promise_type>> promises_by_id_;
+    tsl::robin_map<meta::unique_string, std::vector<promise_type>> promises_by_id_{};
     exec::executor& executor_;
-    entt::registry& registry_;
-    entt::entity entity_;
 };
 
 } // namespace sl::ecs
