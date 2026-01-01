836a42d75e01096ad72911a800d46d26f98b7866
UsatiyNyan <us4tiyny4n@gmail.com>
2026-01-01 16:50:02 +0100

    render: better logging

--- src/graphics/system/render.cpp
+++ src/graphics/system/render.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "sl/game/graphics/system/render.hpp"
+#include "sl/game/detail/log.hpp"
 #include "sl/game/graphics/component/vertex.hpp"
 
 #include <sl/ecs/resource.hpp>
@@ -18,12 +19,14 @@ meta::result<meta::unit, graphics_system::error_type> graphics_system::execute(c
 
     auto* const maybe_shader_resource = layer.registry.try_get<ecs::resource<shader>::ptr_type>(layer.root);
     if (maybe_shader_resource == nullptr) {
+        log::trace("no shader storage");
         return meta::err(error_type::NO_SHADER_STORAGE);
     }
     auto& shader_resource = **maybe_shader_resource;
 
     auto* const maybe_vertex_resource = layer.registry.try_get<ecs::resource<vertex>::ptr_type>(layer.root);
     if (maybe_vertex_resource == nullptr) {
+        log::trace("no vertex storage");
         return meta::err(error_type::NO_VERTEX_STORAGE);
     }
     auto& vertex_resource = **maybe_vertex_resource;
@@ -39,14 +42,22 @@ meta::result<meta::unit, graphics_system::error_type> graphics_system::execute(c
         }
         return sve_map;
     });
+    if (sve_map.empty()) {
+        log::trace("no entities with shader::id and vertex::id found");
+    }
 
     const auto camera_entities = layer.registry.template view<camera, transform>();
+    if (camera_entities.size_hint() == 0) {
+        log::trace("no camera entities found");
+    }
+
     for (const auto& [camera_entity, camera_component, camera_tf] : camera_entities.each()) {
         const auto camera_frame = a_window_frame.for_camera(world, camera_component, camera_tf);
 
         for (const auto& [shader_id, ve_map] : sve_map) {
             auto maybe_shader_component = shader_resource.lookup_unsafe(shader_id);
-            if (!ASSUME_VAL(maybe_shader_component.has_value(), shader_id.string_view())) {
+            if (!maybe_shader_component.has_value()) {
+                log::trace("shader.id={} not found", shader_id.string_view());
                 continue;
             }
             meta::persistent<shader> shader_component = std::move(maybe_shader_component).value();
@@ -63,7 +74,8 @@ meta::result<meta::unit, graphics_system::error_type> graphics_system::execute(c
 
             for (const auto& [vertex_id, entities] : ve_map) {
                 auto maybe_vertex_component = vertex_resource.lookup_unsafe(vertex_id);
-                if (!ASSUME_VAL(maybe_vertex_component.has_value(), vertex_id.string_view())) {
+                if (!maybe_vertex_component.has_value()) {
+                    log::trace("vertex.id={} not found", vertex_id.string_view());
                     continue;
                 }
                 meta::persistent<vertex> vertex_component = std::move(maybe_vertex_component).value();
